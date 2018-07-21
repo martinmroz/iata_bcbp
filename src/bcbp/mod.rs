fcb6db3794208213de0b23d66363900c6a3c97c3
Martin Mroz <martinmroz@gmail.com>
2018-07-21 11:19:00 +0800

    License the repository and move the tests back to where they're supposed to go.

--- src/bcbp/mod.rs
+++ src/bcbp/mod.rs
@@ -1,3 +1,7 @@
+// Copyright (C) 2018 Martin Mroz
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
 
 #[derive(Clone,Eq,PartialEq,Hash,Debug,Default)]
 pub struct Leg {
--- tests/guide.rs
+++ tests/guide.rs
@@ -3,10 +3,4 @@
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
-mod bcbp;
-mod de;
-mod error;
-
-pub use bcbp::{Bcbp, Leg, SecurityData};
-pub use de::from_str_strict;
-pub use error::{Error, Result};
+extern crate iata_bcbp;
